2f76a58669cf8d2c46e15c50a56aad8134b560ad
asell22 <alexanderjsell@gmail.com>
2016-04-29 17:39:34 -0700

    ng-cloak to prevent angular flashing expressions

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 angular.module('voting', [])
 .controller('votingCtrl', function() {
   var self = this;
-  this.title = "Awesome Voting App"
+  this.title = "Awesome Voting App";
+  this.polls = [
+    { title: 'poll1', user: 'user1' },
+    { title: 'poll2', user: 'user2' },
+    { title: 'poll3', user: 'user3' },
+    { title: 'poll4', user: 'user4' },
+    { title: 'poll5', user: 'user5' },
+    { title: 'poll6', user: 'user6' },
+  ]
 })
