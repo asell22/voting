a78891c48fc7bc89b8616a11ab3014a1f9681b2e
asell22 <alexanderjsell@gmail.com>
2016-04-30 10:01:51 -0700

    creating poll functionality in controller scope

--- app.js
+++ app.js
@@ -9,5 +9,12 @@ angular.module('voting', [])
     { title: 'poll4', user: 'user4' },
     { title: 'poll5', user: 'user5' },
     { title: 'poll6', user: 'user6' },
-  ]
+  ];
+  this.count = this.polls.length;
+  this.addPoll = function() {
+    this.count++;
+    this.polls.push(
+      {title: `poll${this.count}`, user: `user${this.count}`}
+    )
+  }
 })
