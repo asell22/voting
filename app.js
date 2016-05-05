8afa84a70a6552a13c9889dba5b5245db9b7c13c
asell22 <alexanderjsell@gmail.com>
2016-05-05 10:15:24 -0700

    create a polls service

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 angular.module('voting', [])
-.controller('votingCtrl', function($scope) {
+.factory('polls', function() {
+  var pollsObject = {
+    polls : [
+      { title: 'poll1', user: 'user1' },
+      { title: 'poll2', user: 'user2' },
+      { title: 'poll3', user: 'user3' }
+    ]
+  }
+
+  return pollsObject;
+})
+.controller('votingCtrl', function($scope, polls) {
   var self = this;
   self.title = "Awesome Voting App";
-  self.polls = [
-    { title: 'poll1', user: 'user1' },
-    { title: 'poll2', user: 'user2' },
-    { title: 'poll3', user: 'user3' },
-    { title: 'poll4', user: 'user4' },
-    { title: 'poll5', user: 'user5' },
-    { title: 'poll6', user: 'user6' },
-  ];
+  self.polls = polls.polls;
   self.count = this.polls.length;
   self.addPoll = function() {
     angular.forEach($scope.pollForm.$error.required, function(field) {
