19a47b6e1a54a45646aa5ca88331124693129b8b
asell22 <alexanderjsell@gmail.com>
2016-05-11 10:37:04 -0700

    validation for voting options

--- app.js
+++ app.js
@@ -43,36 +43,33 @@ angular.module('voting', ['ui.router'])
   self.options = [];
   self.optionCount = 1;
 
-  // functions
-  // var increment = function() {
-  //   option.count += 1;
-  // }
-
   self.addOption = function() {
     self.optionObj = {
       name: self.option,
       count: self.optionCount
     }
-    // increment(self.option)
     self.options.push(self.optionObj);
     self.option = '';
     self.optionCount++;
   }
   self.addPoll = function() {
+    if (self.options.length > 1) {
+      angular.forEach($scope.pollForm.$error.required, function(field) {
+        field.$setTouched();
+      });
 
-    angular.forEach($scope.pollForm.$error.required, function(field) {
-      field.$setTouched();
-    });
+      if ($scope.pollForm.$valid) {
+        polls.polls.push(
+          {title: self.name, user: self.username, options: self.options}
+        )
 
-    if ($scope.pollForm.$valid) {
-      polls.polls.push(
-        {title: self.name, user: self.username}
-      )
-
-      $scope.pollForm.$setUntouched();
-      this.name = '';
-      this.username = '';
-      $state.go('home');
+        $scope.pollForm.$setUntouched();
+        this.name = '';
+        this.username = '';
+        $state.go('home');
+      }
+    } else {
+      angular.element(document).find("h5").addClass('highlight');
     }
   }
 })
